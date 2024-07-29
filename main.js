541d7ca7b99fb5fc962de0534b199218c7907775
Pirkisek <pirkisek@gmail.com>
2024-07-29 09:13:44 +0200

    responsive index.html

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function begging() {
     alert("Well.. you already clicked on ontacts so follow me on instagram & github and pwease join my discord 🙏")
+    console.log("succesfully executed")
 }
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
@@ -8,17 +9,21 @@ const openForm = async () =>  {
     document.getElementById("PopUp").style.display = "block";
     await delay(100);
  begging()   
+ console.log("succesfully executed")
     }
     
     function closeForm() {
     document.getElementById("PopUp").style.display = "none";
+    console.log("succesfully executed")
     }
 
     function gmail() {
         alert("pirkisek@gmail.com")
+        console.log("succesfully executed")
         }
 
         function hire() {
             alert("Idk, im lazy to properly do this page so just go to contacts and text me.")
+            console.log("succesfully executed")
             }
 
