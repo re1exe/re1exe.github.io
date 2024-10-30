07a4b34e93b4307b0c57f67b1540269123e7fc55
Pirkisek <pirkisek@gmail.com>
2024-10-30 20:57:34 +0100

    v.2.0

---
+++ script.js
@@ -0,0 +1,9 @@
+const notifications = document.getElementById('notification');
+
+function closeNotification() {
+ notifications.close();
+}
+function openNotification() {
+    notifications.showModal();
+   }
+   
