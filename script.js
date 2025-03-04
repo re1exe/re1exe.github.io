a640536db2434a4430149f90b122ce9b0584bbba
Rei <rei.exe@hotmail.com>
2025-03-04 20:32:31 +0100

    ain

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("✅ Page loaded!");
+});
+
+  
+  function scrollToTop() {
+        document.getElementById("main").scrollIntoView();
+    };
+
+    function scrollToProjects() {
+        document.getElementById("projects").scrollIntoView();
+    };
+
+    function scrollToAbtMe() {
+        document.getElementById("aboutme").scrollIntoView();
+    };
