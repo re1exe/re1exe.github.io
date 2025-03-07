c0a18e72fab956b684d1fce0d362cb67c4d8d368
Rei <108322453+Pirkisek@users.noreply.github.com>
2025-03-07 15:20:48 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const notifications = document.getElementById('notification');
-
-function closeNotification() {
- notifications.close();
-}
-function openNotification() {
-    notifications.showModal();
-   }
-   
